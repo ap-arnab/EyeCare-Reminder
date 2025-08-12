cda6db4d6ac25a74ce90f785a2aaf9e5e8066b82
ap-arnab <arnabpaul128@gmail.com>
2025-08-12 22:32:05 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+'''
+Every 20 minutes, look at something 20 feet away for 20 seconds. Then close your eyes for 5 sec.
+'''
+from winotify import Notification, audio
+import pyttsx3
+import time
+import os
+
+# Setup speech engine once
+engine = pyttsx3.init()
+# Icon path (absolute path safer than relative)
+icon_path = os.path.abspath("eye_icon.ico")
+if not os.path.exists(icon_path):
+    print("Warning: Icon not found. Notifications will show without an icon.")
+
+repeat_interval = 1200   # every 20 min
+
+try:
+    while True:
+        title = "Reminder!"
+        message = "20-20-20 rule time!!"
+        message_say = "Hey, it's 20 20 20 rule time!"
+        
+        # Show toast
+        toast = Notification(app_id="EyeCare Reminder",
+                            title=title,
+                            msg=message,
+                            duration="long",
+                            icon=icon_path)  # "long"== 25 sec, 20 sec for watching outside
+        toast.set_audio(audio.Default, loop=False)
+        toast.show()
+
+        # Speak message
+        engine.say(message_say)
+        engine.runAndWait()
+
+        time.sleep(repeat_interval)
+
+except KeyboardInterrupt:
+    print("Code stopped by user.")
+
+except Exception as e:
+    print(e)
+    
